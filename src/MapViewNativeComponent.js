a8fbb435d5f5f2a4da92d02a1ac522758463156b
Tomasz Zawadzki <tomasz.zawadzki@swmansion.com>
2022-05-25 15:27:39 +0200

    Setup codegen for native component

---
+++ src/MapViewNativeComponent.js
@@ -0,0 +1,12 @@
+// @flow
+import type {ViewProps} from 'react-native/Libraries/Components/View/ViewPropTypes';
+import type {HostComponent} from 'react-native';
+import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
+
+type NativeProps = $ReadOnly<{|
+  ...ViewProps,
+|}>;
+
+export default (codegenNativeComponent<NativeProps>(
+  'RNMapView',
+): HostComponent<NativeProps>);
