cf425f0a6c376cd5c3833d7a9784f5a474626e1a
Tomasz Zawadzki <tomasz.zawadzki@swmansion.com>
2022-05-25 14:42:13 +0200

    NativeModule for iOS

--- Example/App.tsx
+++ Example/App.tsx
@@ -1,11 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 import React from 'react';
+import { Utils } from 'react-native-appjs';
 
 export default function App() {
+  const handleGetOrientation = () => {
+    console.log('getOrientation', Utils.getOrientation());
+  };
+
+  const handleGetLocation = () => {
+    console.log('getLocation', Utils.getLocation());
+  };
+
+  const handleGetDeviceInfo = () => {
+    console.log('getDeviceInfo', Utils.getDeviceInfo());
+  };
+
+  const handleEncodeData = async () => {
+    const result = await Utils.encode('App.js');
+    console.log('encode', result);
+  };
+
   return (
     <View style={styles.container}>
       <Text>App.js</Text>
+      <Button title="getOrientation" onPress={handleGetOrientation} />
+      <Button title="getLocation" onPress={handleGetLocation} />
+      <Button title="getDeviceInfo" onPress={handleGetDeviceInfo} />
+      <Button title="encode" onPress={handleEncodeData} />
     </View>
   );
 }
